1a927d5139636dbba4ddfe5dcd9eda00d635dd2c
Guy Taylor <thebigguy.co.uk@gmail.com>
2009-12-14 03:40:20 +0000

    Added copyright

--- py_gtk_gpsd.py
+++ py_gtk_gpsd.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python
 
-# example helloworld.py
+"""
+Copyright (c) 2009, Guy Taylor <thebigguy.co.uk@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * The names of the contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL GUY TAYLOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+"""
+This is a new version of the 'xgps' program to try and fulfil this wish
+
+	*** Rewrite xgps
+
+	The Motif/Lesstif dependency is a pain in the butt, the code is a 
+	maintainability problem, and we'd like to extend it to display AIS
+	messages. Probably the thing to do is write something pygtk-based.
+
+The original program is available from "http://gpsd.berlios.de/index.html"
+
+"""
 
 import pygtk
 pygtk.require('2.0')
